6ef66fdc4ef69d0c969607c742fa6955f65c2b8c
Tim Sell <trsell@gmail.com>
2015-05-28 16:40:11 -0400

    use a map for more efficient checking of the incoming attacks

--- labelling.hs
+++ labelling.hs
@@ -6,47 +6,45 @@ import Data.Ord
 main :: IO ()
 main = return ()
 
-data Label a = In a | Out a | Undecided a deriving (Show, Eq)
+data Label a = In a | Out a | Undecided a deriving (Show, Eq, Ord)
 
-data ArgumentFramework a = AF [(a,a)] deriving (Show, Eq)
-
--- record syntax
--- data ArgumentFramework2 a = AF2 (M.Map a [a]) (M.Map a [a])
-data ArgumentFramework2 a = AF2 {
+data ArgumentFramework a = AF {
  forwardAttacks :: Map.Map a [a],
- reverseAttacks :: Map.Map a [a] } deriving (Show, Eq)
+ reverseAttacks :: Map.Map a [a]
+} deriving (Show, Eq, Ord)
 
 sortGroupBy f = groupBy ((==) `on` f) . sortBy (comparing f)
 
-mkAF :: (Ord a) => [(a,a)] -> ArgumentFramework2 a
+mkAF :: (Ord a) => [(a,a)] -> ArgumentFramework a
 mkAF attacks = let
     fwd = (map (\atts -> (fst (head atts), map snd atts)) (sortGroupBy fst attacks))
     rev = (map (\atts -> (snd (head atts), map fst atts)) (sortGroupBy snd attacks))
-    in AF2 (Map.fromList fwd) (Map.fromList rev)
+    in AF (Map.fromList fwd) (Map.fromList rev)
 
-grounded_labelling :: (Show a, Eq a) => ArgumentFramework a -> [Label a] -> [Label a]
+attackers :: (Ord a) => ArgumentFramework a -> a -> [a]
+attackers af a = case (Map.lookup a (reverseAttacks af)) of
+    Just a  -> a
+    Nothing -> []
+
+grounded_labelling :: (Show a, Eq a, Ord a) => ArgumentFramework a -> [Label a] -> [Label a]
 grounded_labelling af labelling =
     let ins = [a | In a <- labelling]
         outs = [a | Out a <- labelling]
         undecided = [a | Undecided a <- labelling]
 
-        ins' = ins ++ [a | a <- undecided, hasNoValidAttacks a outs af]
-        outs' = outs ++ [a | a <- undecided, hasValidAttack a ins af]
+        ins' = ins ++ [a | a <- undecided, hasNoValidAttacks a outs (attackers af a)]
+        outs' = outs ++ [a | a <- undecided, hasValidAttack a ins (attackers af a)]
         undecided' = [a | a <- undecided, notElem a (ins' ++ outs')]
 
-        labelling' = (map In ins')
-            ++ (map Out outs')
-            ++ (map Undecided undecided')
+        labelling' = (map In ins') ++ (map Out outs') ++ (map Undecided undecided')
     in
         if length undecided == length undecided' then
             labelling'
         else
             grounded_labelling af labelling'
 
-hasNoValidAttacks :: (Eq a) => a -> [a] -> ArgumentFramework a -> Bool
-hasNoValidAttacks a outs (AF attacks) =
-    and [ elem x outs | (x,y) <- attacks, y == a]
+hasNoValidAttacks :: (Eq a) => a -> [a] -> [a] -> Bool
+hasNoValidAttacks a outs attackers = and [ elem x outs | x <- attackers]
 
-hasValidAttack :: (Eq a) =>  a -> [a] -> ArgumentFramework a -> Bool
-hasValidAttack a ins (AF attacks) =
-    or [ elem x ins | (x,y) <- attacks, y == a]
+hasValidAttack :: (Eq a) =>  a -> [a] -> [a] -> Bool
+hasValidAttack a ins attackers  = or [ elem x ins | x <- attackers]
